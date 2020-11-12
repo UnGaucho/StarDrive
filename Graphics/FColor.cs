e978b417687a14ca931b7a64a685515ad7ce0e5f
Crunchy Gremlin <crunchygremlin@gmail.com>
2020-11-11 21:41:01 -0800

    copy graphics abstraction from previous pr

---
+++ Graphics/FColor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Ship_Game
+{
+    /// <summary>
+    /// This is equivalent to XNA Color class
+    /// Using F prefix here to differentiate
+    /// </summary>
+    public struct FColor
+    {
+        /// <summary>
+        /// TODO: Currently just reuse XNA implementation to save some time
+        /// but eventually should replace with a custom implementation
+        /// </summary>
+        public Color XnaColor;
+
+        public FColor(Color xnaColor)
+        {
+            XnaColor = xnaColor;
+        }
+
+        public static implicit operator FColor(Color xnaColor)
+        {
+            return new FColor(xnaColor);
+        }
+    }
+}
---
+++ Graphics/FVector2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace Ship_Game
+{
+    /// <summary>
+    /// This is equivalent to XNA Vector2
+    /// Just using the F prefix here to differentiate.
+    /// </summary>
+    public struct FVector2
+    {
+        public float X;
+        public float Y;
+
+        public FVector2(float x, float y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public static implicit operator FVector2(in Vector2 xnaVector)
+        {
+            return new FVector2(xnaVector.X, xnaVector.Y);
+        }
+    }
+}

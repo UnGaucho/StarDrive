4439af6a45e2b8bf60dc3a5f10cbeca33c055ae3
Fizban1978 <gkapulis@gmail.com>
2021-01-22 11:33:10 +0200

    info

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -26,4 +26,4 @@ using System.Runtime.InteropServices;
 [assembly: Guid("b38aad3b-18b8-41a8-b758-0e5614dafc49")]
 [assembly: RuntimeCompatibility(WrapNonExceptionThrows=true)]
 
-[assembly: AssemblyInformationalVersion("Release/Venus_10742 1cf140d1d")]
+[assembly: AssemblyInformationalVersion("Release/Venus_10745 363f99d0a")]
